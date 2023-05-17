07ceebb8d776acc8aea6a830fd6a54699776f3d0
Basichy <ethanmarcg@gmail.com>
2023-05-18 00:17:12 +1200

    Fixed Database to JTable, now shows the information or data in the table

--- src/StudentInfoSystemGUI/JavaConnect.java
+++ src/StudentInfoSystemGUI/JavaConnect.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package StudentInformationSystem;
+package StudentInfoSystemGUI;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,6 +10,7 @@ import java.util.logging.Logger;
  *
  * @author ETHAN
  */
+
 public class JavaConnect {
     public static Connection connectdb()
     {
