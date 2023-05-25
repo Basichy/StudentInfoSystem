3e2280ba1acf81145256648fcbdac069600b3786
Basichy <ethanmarcg@gmail.com>
2023-05-25 15:26:23 +1200

    Set up Exam Jframe and connected main to Exam class

--- src/StudentInfoSystemGUI/Main.java
+++ src/StudentInfoSystemGUI/Main.java
@@ -126,6 +126,11 @@ public class Main extends javax.swing.JFrame {
         });
 
         jButton3.setText("Exam");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("Student");
 
@@ -204,6 +209,12 @@ public class Main extends javax.swing.JFrame {
         s.setVisible(true);
         
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        Exam e = new Exam();
+        e.setVisible(true);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
