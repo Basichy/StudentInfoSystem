12e5095304480ee2a23104c4dbca9bc25f2fb73d
Basichy <ethanmarcg@gmail.com>
2023-06-07 23:51:32 +1200

    Due to changes I have done It led to plenty of errors specially the database connection. There I went back to the previous version of this and this time the changes I have is improve the database connection due to issue earlier and fixed the table creation, as I did not realizes that some were trying to create the same tables...

---
+++ src/StudentInfoSystemGUI/JavaConnect.java
@@ -0,0 +1,26 @@
+package StudentInfoSystemGUI;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author EthanGaylan
+ */
+
+public class JavaConnect {
+    public static Connection connectdb()
+    {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:derby:StudentInfoSystem;create=true");
+            return conn;
+        } catch (SQLException ex) {
+            Logger.getLogger(JavaConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
